a2f2d817bcfae701e954efdd304854eaf00edffb
Dmitry Fedonnikov <d.fedonnikov@gmail.com>
2017-11-14 13:54:53 +0300

    Implemented MealService as a MealServiceImpl

--- src/main/java/ru/javawebinar/topjava/service/MealService.java
+++ src/main/java/ru/javawebinar/topjava/service/MealService.java
@@ -1,4 +1,17 @@
 package ru.javawebinar.topjava.service;
 
+import ru.javawebinar.topjava.model.Meal;
+
+import java.util.List;
+
 public interface MealService {
+    List<Meal> getAll();
+
+    Meal get(int id);
+
+    Meal create(Meal user);
+
+    void delete(int id);
+
+    void update(Meal user);
 }
--- src/main/java/ru/javawebinar/topjava/service/MealServiceImpl.java
+++ src/main/java/ru/javawebinar/topjava/service/MealServiceImpl.java
@@ -1,9 +1,43 @@
 package ru.javawebinar.topjava.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import ru.javawebinar.topjava.model.Meal;
 import ru.javawebinar.topjava.repository.MealRepository;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import static ru.javawebinar.topjava.util.ValidationUtil.checkNotFoundWithId;
+
+@Service
 public class MealServiceImpl implements MealService {
 
+    @Autowired
     private MealRepository repository;
 
+    @Override
+    public List<Meal> getAll() {
+        return new ArrayList<>(repository.getAll());
+    }
+
+    @Override
+    public Meal get(int id) {
+        return checkNotFoundWithId(repository.get(id), id);
+    }
+
+    @Override
+    public Meal create(Meal meal) {
+        return repository.save(meal);
+    }
+
+    @Override
+    public void delete(int id) {
+        checkNotFoundWithId(repository.delete(id), id);
+    }
+
+    @Override
+    public void update(Meal meal) {
+        repository.save(meal);
+    }
 }
