61b5816d960a0beb97baa36f1e53eab4a4d526f5
Dmitry Fedonnikov <d.fedonnikov@gmail.com>
2017-11-22 19:40:47 +0300

    Fixed SpringMain, InMemoryAdminRestControllerTest, InMemoryAdminRestControllerSpringTest

--- src/test/java/ru/javawebinar/topjava/web/InMemoryAdminRestControllerTest.java
+++ src/test/java/ru/javawebinar/topjava/web/InMemoryAdminRestControllerTest.java
@@ -3,6 +3,7 @@ package ru.javawebinar.topjava.web;
 import org.junit.*;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.test.context.ContextConfiguration;
 import ru.javawebinar.topjava.UserTestData;
 import ru.javawebinar.topjava.model.User;
 import ru.javawebinar.topjava.repository.mock.InMemoryUserRepositoryImpl;
@@ -20,7 +21,7 @@ public class InMemoryAdminRestControllerTest {
 
     @BeforeClass
     public static void beforeClass() {
-        appCtx = new ClassPathXmlApplicationContext("spring/spring-app.xml");
+        appCtx = new ClassPathXmlApplicationContext("spring/spring-app.xml", "spring/spring-db.xml");
         System.out.println("\n" + Arrays.toString(appCtx.getBeanDefinitionNames()) + "\n");
         controller = appCtx.getBean(AdminRestController.class);
     }
