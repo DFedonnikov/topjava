b854fc9ff66a46485e4993a2ee9f111729ae3a1b
Dmitry Fedonnikov <d.fedonnikov@gmail.com>
2017-11-22 19:41:02 +0300

    Initiated and populated meals table

--- src/main/resources/db/populateDB.sql
+++ src/main/resources/db/populateDB.sql
@@ -1,3 +1,4 @@
+DELETE FROM meals;
 DELETE FROM user_roles;
 DELETE FROM users;
 ALTER SEQUENCE global_seq RESTART WITH 100000;
@@ -9,3 +10,7 @@ INSERT INTO users (name, email, password) VALUES
 INSERT INTO user_roles (role, user_id) VALUES
   ('ROLE_USER', 100000),
   ('ROLE_ADMIN', 100001);
+
+INSERT INTO meals (datetime, description, calories, user_id) VALUES
+  ('22/11/2017 18:26', 'Breakfast', 500, 100000),
+  ('22/11/2017 18:28', 'Launch', 800, 100001)
